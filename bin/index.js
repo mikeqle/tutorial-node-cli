a745cfb78ef08ffa4476f26d46a2d1db9daeafa0
Mike Le <mike.le@live.com>
2023-11-06 13:35:11 -0800

    UPDATE handling specific request

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log("Test CLI")
+const fs = require("fs");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+const [type, name] = argv._;
+
+if (type === "make:controller") {
+  try {
+    fs.readFileSync("controllers/" + name + ".js");
+    console.log("File exists");
+  } catch (error) {
+    fs.writeFileSync("controllers/" + name + ".js", "Hello", { flag: "w+" });
+    console.log("Created Controller" + name);
+  }
+}
